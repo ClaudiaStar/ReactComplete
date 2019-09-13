94ee38ce91b48c95799b14b42b412cf47284e71f
Claudia Estrada <cgestrada518@gmail.com>
2019-09-13 13:42:09 -0700

    include conditional statement to toggle rendering of persons components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ class App extends Component {
       { name: 'Claudia', age: 30 },
       { name: 'Chris', age: 32 },
       { name: 'Gael', age: 3 }
-    ]
+    ],
+    otherState: 'some other value',
+    // Boolean that will be used in togglerPersonsHandler
+    showPersons: false
   };
 
   // Function that handles the onClick event for the button below
@@ -32,6 +35,15 @@ class App extends Component {
       { name: 'Gael', age: 32 }
     ] })
   }
+
+  // Toggler Persons Button Handler which will show and hide Person components each time it is clicked
+  togglerPersonsHandler = (event) => {
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons: !doesShow});
+  }
+
+
+
   render() {
 
     // Can also use in-line styles. Changing props on hover is hard to do with in-line though.
@@ -43,6 +55,31 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    // Conditional statement which tells the button to either hide or show the Person components
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+      <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age} />
+      <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+      // changed is arbitrary name given to the new prop which will be tied to the input element in Person component
+        changed={this.nameChangedHandler}
+      />
+      <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}
+      // You can pass methods as props as shown below
+      // You can also pass arguments with bind(this, data)
+        click={this.switchNameHandler.bind(this, 'Cloud!')}>Otherwise known as silly canoot.</Person>
+      </div>
+      );
+    }
+
     return (
       <div className = "App">
       <h1> Hi, I'm a React App</h1>
@@ -50,23 +87,8 @@ class App extends Component {
 
       <button
       style={style}
-      onClick={() => this.switchNameHandler('Claudia!')}>Swith Name</button>
-
-      <Person
-      name={this.state.persons[0].name}
-      age={this.state.persons[0].age} />
-      <Person
-      name={this.state.persons[1].name}
-      age={this.state.persons[1].age}
-      // changed is arbitrary name given to the new prop which will be tied to the input element in Person component
-      changed={this.nameChangedHandler}
-      />
-      <Person
-      name={this.state.persons[2].name}
-      age={this.state.persons[2].age}
-      // You can pass methods as props as shown below
-      // You can also pass arguments with bind(this, data)
-      click={this.switchNameHandler.bind(this, 'Cloud!')}>Otherwise known as silly canoot.</Person>
+      onClick={this.togglerPersonsHandler}>Toggler Persons</button>
+      {persons}
       </div>
     );
   }

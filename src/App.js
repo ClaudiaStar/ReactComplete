8808a2f9529aefeac7d9bac250e0955eeaef08a4
Claudia Estrada <cgestrada518@gmail.com>
2019-09-18 13:26:23 -0700

    Add styles using Radium

--- src/App.js
+++ src/App.js
@@ -1,40 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
+// Radium allows usage of pseudo selectors & media queries
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+
 
 
   // This is the default way of managing state in React (with a class component). Later on we will learn about using hooks to handle state in functional components
 class App extends Component {
   state = {
     persons: [
-      { name: 'Claudia', age: 30 },
-      { name: 'Chris', age: 32 },
-      { name: 'Gael', age: 3 }
+      { id: 'akhakhf', name: 'Claudia', age: 30 },
+      { id: 'akghahg', name: 'Chris', age: 32 },
+      { id: 'jfajgal', name: 'Gael', age: 3 }
     ],
     otherState: 'some other value',
     // Boolean that will be used in togglerPersonsHandler
     showPersons: false
   };
 
-  // Function that handles the onClick event for the button below
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DONT DO THIS: this.state.persons[0].name = 'Claudia Giovanna'
-    this.setState({persons: [
-      { name: newName, age: 3 },
-      { name: 'Chris', age: 30 },
-      { name: 'Gael', age: 32 }
-    ] })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+    ...this.state.persons[personIndex]
+  };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      { name: 'Claudia', age: 3 },
-      // event.target.value access the value typed in the input element
-      { name: event.target.value, age: 30 },
-      { name: 'Gael', age: 32 }
-    ] })
+  deletePersonHandler = (personIndex) => {
+    // The slice() method below returns the selected elements in an array, as a new array object.
+    // const persons = this.state.persons.slice();
+    // The spread operator method is another way to make a copy of an old array & more modern.
+    const persons = [...this.state.persons];
+    // The splice method below removes array element at personIndex (1 means remove);
+    persons.splice(personIndex, 1);
+    // A new person array is returned and set as new state
+    this.setState({persons: persons});
   }
+
+
+
 
   // Toggler Persons Button Handler which will show and hide Person components each time it is clicked
   togglerPersonsHandler = (event) => {
@@ -48,50 +63,73 @@ class App extends Component {
 
     // Can also use in-line styles. Changing props on hover is hard to do with in-line though.
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      // pseudo selectors are available now since Radium was imported
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
 
-    // Conditional statement which tells the button to either hide or show the Person components
+
     if (this.state.showPersons) {
       persons = (
+// Person components rendered as list by mapping an array instead of one by one as done previously
         <div>
-      <Person
-        name={this.state.persons[0].name}
-        age={this.state.persons[0].age} />
-      <Person
-        name={this.state.persons[1].name}
-        age={this.state.persons[1].age}
-      // changed is arbitrary name given to the new prop which will be tied to the input element in Person component
-        changed={this.nameChangedHandler}
-      />
-      <Person
-        name={this.state.persons[2].name}
-        age={this.state.persons[2].age}
-      // You can pass methods as props as shown below
-      // You can also pass arguments with bind(this, data)
-        click={this.switchNameHandler.bind(this, 'Cloud!')}>Otherwise known as silly canoot.</Person>
+          {this.state.persons.map((person, index)=> {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
+            />
+          })}
       </div>
       );
+      //set button background color to red when persons is active
+      style.backgroundColor ='red';
+      // syntax to use pseudo selector thanks to Radium
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+
     }
 
+    const classes = [];
+    if ( this.state.persons.length <=2 ) {
+      classes.push('red');
+      // classes = [red]
+    }
+    if ( this.state.persons.length <= 1 ) {
+      classes.push('bold');
+      // classes = [bold]
+    }
+
+
     return (
+      // Must wrap app in StyleRoot tags whenever using Radium for media queries or keyframes
+      <StyleRoot>
       <div className = "App">
       <h1> Hi, I'm a React App</h1>
-      <p>This is really working!</p>
+      <p className={classes.join(' ')}>This is really working!</p>
 
       <button
       style={style}
       onClick={this.togglerPersonsHandler}>Toggler Persons</button>
       {persons}
       </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,21 @@
 // ES6 functions hold advantage especially with the "this" keyword
 
 import React from 'react';
+import Radium from 'radium';
 import './Person.css';
 
+
 const person = (props) => {
+  // media query using Radium
+  const style = {
+    '@media (min-width: 600px)': {
+      width: '450px'
+    }
+  }
+
+
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
     <p onClick={props.click}>My name is {props.name} and I am {props.age} years old.</p>
     <p>{props.children}</p>
     {/*Two way binding to the input element since we have an original value and a an onChange value*/}
@@ -16,4 +26,4 @@ const person = (props) => {
   );
 };
 
-export default person;
+export default Radium(person);
